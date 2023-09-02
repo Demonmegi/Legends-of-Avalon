8ce096c2ea4f85cfdf665c71331879e79db03f9b
Yakut58 <66906645+Yakut58@users.noreply.github.com>
2023-09-02 14:54:14 +0200

    Update translations.sql

--- db/translations.sql
+++ db/translations.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 01, 2023 at 10:22 PM
+-- Generation Time: Sep 02, 2023 at 02:54 PM
 -- Server version: 10.5.19-MariaDB-0+deb11u2
 -- PHP Version: 8.1.13
 
