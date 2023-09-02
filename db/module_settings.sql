08fcf77ea1be37fa7b31b21648cc71179b8cbf2d
Yakut58 <66906645+Yakut58@users.noreply.github.com>
2023-09-02 11:45:41 +0200

    Create module_settings.sql

---
+++ db/module_settings.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 02, 2023 at 11:43 AM
+-- Server version: 10.5.19-MariaDB-0+deb11u2
+-- PHP Version: 8.1.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loa_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `module_settings`
+--
+
+CREATE TABLE `module_settings` (
+  `modulename` varchar(50) NOT NULL,
+  `setting` varchar(50) NOT NULL,
+  `value` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `module_settings`
+--
+
+INSERT INTO `module_settings` (`modulename`, `setting`, `value`) VALUES
+('dpafterdk', 'maxdp1', '20'),
+('dpafterdk', 'maxdp2', '35'),
+('dpafterdk', 'maxdp3', '75'),
+('expbar', 'showbar', '1'),
+('expbar', 'showexpnumber', '1'),
+('expbar', 'shownextgoal', '1'),
+('healthbar', 'showbar', '1'),
+('healthbar', 'showcurrent', '1'),
+('healthbar', 'showmax', '1'),
+('specialtyarcher', 'mindk', '0'),
+('specialtyfighter', 'cost', '0'),
+('specialtyfighter', 'mindk', '0'),
+('translationwizard', 'autoscan', '0'),
+('translationwizard', 'blockcentral', '0'),
+('translationwizard', 'blocktrans', '0'),
+('translationwizard', 'lookuppath', 'http://translations.nb-core.org'),
+('translationwizard', 'page', '20'),
+('translationwizard', 'query', '0'),
+('translationwizard', 'restricted', '0'),
+('translationwizard', 'translationdelete', '0');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `module_settings`
+--
+ALTER TABLE `module_settings`
+  ADD PRIMARY KEY (`modulename`,`setting`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
