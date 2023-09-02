f4181cd2f2445ca5638d16ad8eacad5145b39da1
Yakut58 <66906645+Yakut58@users.noreply.github.com>
2023-09-02 02:04:16 +0200

    translate masters table

--- db/masters.sql
+++ db/masters.sql
@@ -46,20 +46,20 @@ CREATE TABLE `masters` (
 --
 
 INSERT INTO `masters` (`creatureid`, `creaturename`, `creaturelevel`, `creatureweapon`, `creaturelose`, `creaturewin`, `creaturegold`, `creatureexp`, `creaturehealth`, `creatureattack`, `creaturedefense`) VALUES
-(1, 'Mireraband', 1, 'Small Dagger', 'Well done %w`&, I should have guessed you\'d grown some.', 'As I thought, %w`^, your skills are no match for my own!', NULL, NULL, 12, 2, 2),
-(2, 'Fie', 2, 'Short Sword', 'Well done %w`&, you really know how to use your %x.', 'You should have known you were no match for my %X', NULL, NULL, 22, 4, 4),
-(3, 'Glynyc', 3, 'Hugely Spiked Mace', 'Aah, defeated by the likes of you!  Next thing you know, Mireraband will be hunting me down!', 'Haha, maybe you should go back to Mireraband\'s class.', NULL, NULL, 33, 6, 6),
-(4, 'Guth', 4, 'Spiked Club', 'Ha!  Hahaha, excellent fight %w`&!  Haven\'t had a battle like that since I was in the RAF!', 'Back in the RAF, we\'d have eaten the likes of you alive!  Go work on your skills some old boy!', NULL, NULL, 44, 8, 8),
-(5, 'Unï¿½lith', 5, 'Thought Control', 'Your mind is greater than mine.  I concede defeat.', 'Your mental powers are lacking.  Meditate on this failure and perhaps some day you will defeat me.', NULL, NULL, 55, 10, 10),
-(6, 'Adwares', 6, 'Dwarven Battle Axe', 'Ach!  Y\' do hold yer %x with skeel!', 'Har!  Y\' do be needin moore praktise y\' wee cub!', NULL, NULL, 66, 12, 12),
-(7, 'Gerrard', 7, 'Battle Bow', 'Hmm, mayhaps I underestimated you.', 'As I thought.', NULL, NULL, 77, 14, 14),
-(8, 'Ceiloth', 8, 'Orkos Broadsword', 'Well done %w`&, I can see that great things lie in the future for you!', 'You are becoming powerful, but not yet that powerful.', NULL, NULL, 88, 16, 16),
-(9, 'Dwiredan', 9, 'Twin Swords', 'Perhaps I should have considered your %x...', 'Perhaps you\'ll reconsider my twin swords before you try that again?', NULL, NULL, 99, 18, 18),
-(10, 'Sensei Noetha', 10, 'Martial Arts Skills', 'Your style was superior, your form greater.  I bow to you.', 'Learn to adapt your style, and you shall prevail.', NULL, NULL, 110, 20, 20),
-(11, 'Celith', 11, 'Throwing Halos', 'Wow, how did you dodge all those halos?', 'Watch out for that last halo, it\'s coming back this way!', NULL, NULL, 121, 22, 22),
-(12, 'Gadriel the Elven Ranger', 12, 'Elven Long Bow', 'I can accept that you defeated me, because after all elves are immortal while you are not, so the victory will be mine.', 'Do not forget that elves are immortal.  Mortals will likely never defeat one of the fey.', NULL, NULL, 132, 24, 24),
-(13, 'Adoawyr', 13, 'Gargantuan Broad Sword', 'If I could have picked up this sword, I probably would have done better!', 'Haha, I couldn\'t even pick the sword UP and I still won!', NULL, NULL, 143, 26, 26),
-(14, 'Yoresh', 14, 'Death Touch', 'Well, you evaded my touch.  I salute you!', 'Watch out for my touch next time!', NULL, NULL, 154, 28, 28);
+(1, 'Mireraband', 1, 'Kleines Dolch', 'Gut gemacht %w`&, ich hätte erraten sollen, dass du gewachsen bist.', 'Wie ich dachte, %w`^, deine Fähigkeiten sind nicht mit meinen eigenen zu vergleichen!', NULL, NULL, 12, 2, 2),
+(2, 'Fie', 2, 'Kurzschwert', 'Gut gemacht %w`&, du weißt wirklich, wie man dein %x benutzt.', 'Du hättest wissen sollen, dass du meinem %X nicht gewachsen bist.', NULL, NULL, 22, 4, 4),
+(3, 'Glynyc', 3, 'Hochspitzige Keule', 'Aah, von jemandem wie dir besiegt! Als nächstes wird Mireraband mich jagen!', 'Haha, vielleicht solltest du zurück in Mirerabands Klasse gehen.', NULL, NULL, 33, 6, 6),
+(4, 'Guth', 4, 'Spitzknüppel', 'Ha! Hahaha, ausgezeichneter Kampf %w`&! Ich hatte keinen solchen Kampf mehr seit meiner Zeit bei der RAF!', 'In der RAF hätten wir solche wie dich lebendig gefressen! Arbeite an deinen Fähigkeiten, alter Junge!', NULL, NULL, 44, 8, 8),
+(5, 'Unï¿½lith', 5, 'Gedankenkontrolle', 'Dein Geist ist größer als meiner. Ich gebe mich geschlagen.', 'Deine mentalen Kräfte lassen zu wünschen übrig. Meditiere über dieses Versagen, und vielleicht wirst du mich irgendwann besiegen.', NULL, NULL, 55, 10, 10),
+(6, 'Adwares', 6, 'Zwergenkampfaxt', 'Ach! Ihr haltet euer %x mit Geschick!', 'Har! Ihr braucht noch mehr Übung, ihr kleiner Lausbub!', NULL, NULL, 66, 12, 12),
+(7, 'Gerrard', 7, 'Kampfbogen', 'Hmm, vielleicht habe ich dich unterschätzt.', 'Wie ich dachte.', NULL, NULL, 77, 14, 14),
+(8, 'Ceiloth', 8, 'Orkos Breitschwert', 'Gut gemacht %w`&, ich kann sehen, dass große Dinge in deiner Zukunft liegen!', 'Du wirst mächtig, aber noch nicht so mächtig.', NULL, NULL, 88, 16, 16),
+(9, 'Dwiredan', 9, 'Zwillingsschwerter', 'Vielleicht hätte ich dein %x in Betracht ziehen sollen...', 'Vielleicht überlegst du es dir, meine Zwillingsschwerter zu benutzen, bevor du das nochmal versuchst?', NULL, NULL, 99, 18, 18),
+(10, 'Sensei Noetha', 10, 'Kampfkunstfähigkeiten', 'Dein Stil war überlegen, deine Form größer. Ich verneige mich vor dir.', 'Lerne, deinen Stil anzupassen, und du wirst siegen.', NULL, NULL, 110, 20, 20),
+(11, 'Celith', 11, 'Werfende Halos', 'Wow, wie hast du all diesen Halos ausweichen können?', 'Pass auf den letzten Halo auf, er kommt zurück!', NULL, NULL, 121, 22, 22),
+(12, 'Gadriel der Elfenranger', 12, 'Elfenlangbogen', 'Ich kann akzeptieren, dass du mich besiegt hast, denn schließlich sind Elfen unsterblich, während du es nicht bist, also wird der Sieg meiner sein.', 'Vergiss nicht, dass Elfen unsterblich sind. Sterbliche werden wahrscheinlich nie einen der Feen besiegen.', NULL, NULL, 132, 24, 24),
+(13, 'Adoawyr', 13, 'Gigantisches Breitschwert', 'Wenn ich dieses Schwert hätte aufheben können, hätte ich wahrscheinlich besser abgeschnitten!', 'Haha, ich konnte das Schwert nicht einmal aufheben, und ich habe trotzdem gewonnen!', NULL, NULL, 143, 26, 26),
+(14, 'Yoresh', 14, 'Todesberührung', 'Nun, du hast meiner Berührung ausgewichen. Ich verneige mich vor dir!', 'Pass das nächste Mal auf meine Berührung auf!', NULL, NULL, 154, 28, 28);
 
 --
 -- Indexes for dumped tables
