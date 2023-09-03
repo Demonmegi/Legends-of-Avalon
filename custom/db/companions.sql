c32f99335c7e598a9b8892de7bb4b3b274efb550
Yakut58 <66906645+Yakut58@users.noreply.github.com>
2023-09-03 17:11:46 +0200

    update tables

--- custom/db/companions.sql
+++ custom/db/companions.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 03, 2023 at 04:01 PM
+-- Generation Time: Sep 03, 2023 at 04:41 PM
 -- Server version: 10.5.19-MariaDB-0+deb11u2
 -- PHP Version: 8.1.13
 
